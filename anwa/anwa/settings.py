7a8686b5a6b0a7d63869690a98584bc541946637
AndresR <guepardo2008@hotmail.com>
2025-02-13 20:56:09 -0500

    feat: add cors

--- anwa/anwa/settings.py
+++ anwa/anwa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'anwa.api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'anwa.urls'
 
