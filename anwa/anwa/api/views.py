cdee470be8ade93e83e4fc7114ab32b64ff96bc3
AndresR <guepardo2008@hotmail.com>
2025-02-18 21:18:33 -0500

    fix: only users with permisions can use api

--- anwa/anwa/api/views.py
+++ anwa/anwa/api/views.py
@@ -30,7 +30,7 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     serializer_class = UsuarioSerializer
     permission_classes = [permissions.IsAuthenticated]  # Solo usuarios autenticados
 
-    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated, permissions.DjangoModelPermissions])
     def perfil(self, request):
         """Devuelve los datos del usuario autenticado"""
         usuario = request.user  # Obtiene el usuario autenticado
