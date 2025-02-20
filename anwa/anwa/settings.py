1d363912cfb59807569f13a36934ffffe4a606e3
AndresR <guepardo2008@hotmail.com>
2025-02-19 19:59:27 -0500

    fix:setup permisions

--- anwa/anwa/settings.py
+++ anwa/anwa/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',  # Asegura autenticación
+       # 'rest_framework.permissions.IsAuthenticated',  # Asegura autenticación
         'rest_framework.permissions.DjangoModelPermissions',  # Restringe acceso sin permisos
     ),
 }
