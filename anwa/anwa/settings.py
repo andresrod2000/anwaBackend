86a7a6d3e039c1e80261cf01cbd976fded4f91b6
AndresR <guepardo2008@hotmail.com>
2025-02-25 20:39:28 -0500

    feat: add feature

--- anwa/anwa/settings.py
+++ anwa/anwa/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
+#Un comentario
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5x^uot&ezw(tj4xab!e+m1z25ghng66&@ptzdw3+t&i2#3$i^-'
 
