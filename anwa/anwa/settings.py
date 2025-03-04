1057d2fb3287d859e972a16c78dca54bb3681d6c
AndresR <guepardo2008@hotmail.com>
2025-03-03 20:29:52 -0500

    feat: add token expire time

--- anwa/anwa/settings.py
+++ anwa/anwa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -177,3 +177,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'api.Usuario'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  # Por ejemplo, 30 minutos
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # Por ejemplo, 7 días
+}
